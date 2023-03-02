539b11122c7d1d5e71f29e30acd2c9e25acf4b04
fededomm <fededomm@outlook.it>
2023-03-02 17:10:44 +0100

    main, banner and config file

--- main.go
+++ main.go
@@ -1,3 +1,21 @@
 package main
 
-func main()
+import (
+	"fmt"
+	"log"
+
+	"login-backend/rest"
+)
+
+var banner []byte
+
+//go:embed banner.txt
+func main() {
+	cfg, err := ReadConfig()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println(string(banner))
+	rest.Init(cfg.App.ServiceName, &cfg.App.GinRouter)
+}
